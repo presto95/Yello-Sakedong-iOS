9639915c34749c9d2614813db76dee784a7b8769
presto <yoohan95@gmail.com>
2019-02-16 16:13:11 +0900

    networkmanager

--- YelloSakedong/Manager/NetworkManager.swift
+++ YelloSakedong/Manager/NetworkManager.swift
@@ -7,3 +7,38 @@
 //
 
 import Foundation
+
+import Alamofire
+import SwiftyJSON
+
+protocol NetworkManagerType: class {
+  func request(_ urlString: String,
+               method: HTTPMethod,
+               parameters: [String: Any],
+               headers: [String: String],
+               completion: @escaping (JSON?, Int?, Error?) -> Void)
+}
+
+final class NetworkManager: NetworkManagerType {
+  
+  static let shared = NetworkManager()
+  
+  func request(_ urlString: String,
+               method: HTTPMethod = .get,
+               parameters: [String: Any] = [:],
+               headers: [String: String] = [:],
+               completion: @escaping (JSON?, Int?, Error?) -> Void) {
+    Alamofire
+      .request(urlString,
+               method: method,
+               parameters: parameters,
+               encoding: JSONEncoding.default,
+               headers: headers)
+      .responseJSON { response in
+        let json = JSON(response)
+        let statusCode = response.response?.statusCode
+        let error = response.error
+        completion(json, statusCode, error)
+    }
+  }
+}
